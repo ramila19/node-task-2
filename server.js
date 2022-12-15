e9f1ec61a15acbb60fd9a6becc965511d17e8665
unknown <ramilahsynva@gmail.com>
2022-12-15 23:58:38 +0400

    ramila

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Abdulov Servet");
+  res.send("Ramila Huseynova");
 });
 app.get("/card", function (req, res) {
   res.send("Bu route kartlar üçün cavabdehdir");
